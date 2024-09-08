d291395b00a29bdd1bfc49e24ec05a9111ab17cc
mukesh <yryadavmukesh@gmail.com>
2024-09-08 12:41:22 +0530

    First commit

---
+++ ankitarempo-ME_QCALC_QA-master/app/src/main/java/com/crio/qcalc/StandardCalculator.java
@@ -0,0 +1,86 @@
+package com.crio.qcalc;
+
+public class StandardCalculator {
+
+    protected double result;
+
+    public static void getVersion(){
+        System.out.println("Standard Calculator 1.0");
+    }
+
+    // getter
+    public double getResult() {
+        return result;
+    }
+
+    // setter
+    public void setResult(double result) {
+        if (result != 0) {
+            clearResult();
+        }
+        this.result = result;
+    }
+
+    public void clearResult() {
+        result = 0;
+    }
+
+    public void printResult() {
+        System.out.println("Standard Calculator Result:"+ result);
+    }
+
+    public final void add(int num1, int num2) {
+        add((double)num1, (double)num2);
+        // result = num1 + num2;
+    }
+
+    
+    public void add(double num1, double num2){
+        double result = num1 + num2;
+        if((result == Double.MAX_VALUE) || (result == Double.POSITIVE_INFINITY)){
+            throw new ArithmeticException("Double overflow");
+        }
+        this.result = result;
+    }
+
+
+    public final void subtract(int num1, int num2) {
+        subtract((double) num1, (double) num2);
+        //result = num1 - num2;
+    }
+
+    
+    public void subtract(double num1, double num2){
+        double result = num1 - num2;
+        if((result == -Double.MAX_VALUE) || (result == Double.NEGATIVE_INFINITY)){
+            throw new ArithmeticException("Double overflow");
+        }
+        this.result = result;
+    }
+
+
+    public final void multiply(int num1, int num2) {
+        multiply((double) num1, (double) num2);
+        //result = num1 * num2;
+    }
+
+    public void multiply(double num1, double num2) {
+        double result = num1 * num2;
+        if((result == Double.MAX_VALUE) || (result == Double.POSITIVE_INFINITY) || (result == Double.NEGATIVE_INFINITY)){
+            throw new ArithmeticException("Double overflow");
+        }
+        this.result = result;
+    }
+
+    public final void divide(int num1, int num2) {
+        divide((double) num1, (double) num2);
+        //result = num1 / num2;
+    }
+
+    public void divide(double num1, double num2) {
+        if (num2 == 0.0) {
+            throw new ArithmeticException("Divide By Zero");
+        }
+        result = num1 / num2;
+    }
+}
